e193975042c4bd3c292424cd6ea9a61b00dbe7de
Jan Brodda <github@janbrodda.de>
2017-12-12 08:31:43 +0000

    added webproxy scripts

---
+++ webproxy/_source.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+set -e
+
+RESOLVER=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
+
+function clear_file {
+ rm $1 2>/dev/null || true
+}
+
+function http_to_https {
+HOSTNAME=$1
+cat >> $CONFIG <<EOL
+server {
+ listen 80;
+ listen [::]:80;
+ server_name $HOSTNAME;
+ return 301 https://\$host\$request_uri;
+}
+EOL
+}
+
+function https_wildcard {
+ TARGET=$1
+ https_generic_overwrite _ \$host $TARGET
+}
+
+function https_generic {
+ HOSTNAME=$1
+ TARGET=$2
+ _https $HOSTNAME $HOSTNAME $DEFAULT_SSL_HOST $TARGET
+}
+
+function https_generic_overwrite {
+ HOSTNAME=$1
+ OVERWRITEHOST=$2
+ TARGET=$3
+ _https $HOSTNAME $OVERWRITEHOST $DEFAULT_SSL_HOST $TARGET
+}
+
+function https_dedicated {
+ HOSTNAME=$1
+ TARGET=$2
+ _https $HOSTNAME $HOSTNAME $HOSTNAME $TARGET
+}
+
+function https_dedicated_overwrite {
+ HOSTNAME=$1
+ OVERWRITEHOST=$2
+ TARGET=$3
+ _https $HOSTNAME $OVERWRITEHOST $OVERWRITEHOST $TARGET
+}
+
+function https_dedicated_sslname {
+ HOSTNAME=$1
+ SSLHOST=$2
+ TARGET=$3
+ _https $HOSTNAME $HOSTNAME $SSLHOST $TARGET
+}
+
+function https_redirect {
+ HOSTNAME=$1
+ TARGET=$2
+cat >> $CONFIG <<EOL
+server {
+ listen 443;
+ listen [::]:443;
+ server_name $HOSTNAME;
+ ssl on;
+ ssl_certificate $SSL_CONFIG_DIR/$SSL_DEFAULT_HOST/fullchain.cer;
+ ssl_certificate_key $SSL_CONFIG_DIR/$SSL_DEFAULT_HOST/$SSL_DEFAULT_HOST.key;
+ return 302 $TARGET;
+}
+EOL
+}
+
+function _https {
+HOSTNAME=$1
+OVERWRITEHOST=$2
+SSLHOSTFILE=$3
+TARGET=$4
+cat >> $CONFIG <<EOL
+server {
+ listen 443;
+ listen [::]:443;
+ server_name $HOSTNAME;
+ ssl on;
+ ssl_certificate $SSL_CONFIG_DIR/$SSLHOSTFILE/fullchain.cer;
+ ssl_certificate_key $SSL_CONFIG_DIR/$SSLHOSTFILE/$SSLHOSTFILE.key;
+ location / {
+  resolver $RESOLVER ipv6=off;
+  proxy_pass $TARGET\$request_uri;
+  set_real_ip_from 103.21.244.0/22;
+  set_real_ip_from 103.22.200.0/22;
+  set_real_ip_from 103.31.4.0/22;
+  set_real_ip_from 104.16.0.0/12;
+  set_real_ip_from 108.162.192.0/18;
+  set_real_ip_from 131.0.72.0/22;
+  set_real_ip_from 141.101.64.0/18;
+  set_real_ip_from 162.158.0.0/15;
+  set_real_ip_from 172.64.0.0/13;
+  set_real_ip_from 173.245.48.0/20;
+  set_real_ip_from 188.114.96.0/20;
+  set_real_ip_from 190.93.240.0/20;
+  set_real_ip_from 197.234.240.0/22;
+  set_real_ip_from 198.41.128.0/17;
+  set_real_ip_from 199.27.128.0/21;
+  set_real_ip_from 2400:cb00::/32;
+  set_real_ip_from 2606:4700::/32;
+  set_real_ip_from 2803:f800::/32;
+  set_real_ip_from 2405:b500::/32;
+  set_real_ip_from 2405:8100::/32;
+  set_real_ip_from 2c0f:f248::/32;
+  set_real_ip_from 2a06:98c0::/29;
+  real_ip_header CF-Connecting-IP;
+  proxy_set_header Host $OVERWRITEHOST;
+  proxy_set_header X-Real-IP \$remote_addr;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header Accept-Encoding "";
+  proxy_set_header Authorization \$http_authorization;
+  proxy_pass_header Authorization;
+ }
+}
+EOL
+}
---
+++ webproxy/generate.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+. $DIR/_source.sh
+
+CONFIG=/etc/nginx/sites-enabled/webproxy-generated
+DEFAULT_SSL_HOST=root.radiohitwave.com
+SSL_CONFIG_DIR=/root/.acme.sh/
+
+clear_file $CONFIG
+
+http_to_https _
+https_wildcard https://ajenti.lxd
+
+http_to_https ajenti.radiohitwave.com
+https_generic ajenti.radiohitwave.com https://ajenti.lxd:8000
+
+http_to_https seafile.radiohitwave.com
+https_generic seafile.radiohitwave.com https://seafile.lxd
+
+#http_to_https landscape.radiohitwave.com
+#https_generic landscape.radiohitwave.com https://landscape.lxd
+
+#http_to_https cockpit.radiohitwave.com
+#https_generic cockpit.radiohitwave.com https://cockpit.lxd:9090
+
+http_to_https webssh.radiohitwave.com
+https_generic webssh.radiohitwave.com https://webssh.lxd:4200
+
+http_to_https streamripper.radiohitwave.com
+https_generic streamripper.radiohitwave.com http://streamripper.lxd:8000
+
+http_to_https etherpad.radiohitwave.com
+https_generic etherpad.radiohitwave.com http://etherpad.lxd:9001
+
+http_to_https teambutler.radiohitwave.com
+https_generic_overwrite teambutler.radiohitwave.com radiohitwave.teambutler.net https://radiohitwave.teambutler.net
+
+http_to_https mailcow.radiohitwave.com
+
+https_dedicated mailcow.radiohitwave.com https://mailcow.lxd
+#https_dedicated_sslname autoconfig.radiohitwave.com mailcow.radiohitwave.com https://mailcow.lxd
+#https_dedicated_sslname autodiscover.radiohitwave.com mailcow.radiohitwave.com https://mailcow.lxd
+
+#https_dedicated mailcow.radiohitwave.com https://mailcow-dockerized.lxd
+#https_dedicated_sslname autoconfig.radiohitwave.com mailcow.radiohitwave.com https://mailcow-dockerized.lxd
+#https_dedicated_sslname autodiscover.radiohitwave.com mailcow.radiohitwave.com https://mailcow-dockerized.lxd
+
+http_to_https sogo.radiohitwave.com
+https_redirect sogo.radiohitwave.com https://mailcow.radiohitwave.com/SOGo
+
+http_to_https mailcow-dockerized.radiohitwave.com
+https_dedicated_overwrite mailcow-dockerized.radiohitwave.com mailcow.radiohitwave.com https://mailcow-dockerized.lxd
+
+nginx -t
+service nginx reload
+echo NGINX config successfully reloaded
