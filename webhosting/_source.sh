9e7e3d255b213b8d7c3bd5827fd5719caa53becf
Jan Brodda <git@janbrodda.de>
2018-10-11 11:18:41 +0000

    generate dedicated php-fpm pools for each vhost to enable different PHP settings

--- webhosting/_source.sh
+++ webhosting/_source.sh
@@ -7,21 +7,24 @@ cat <<EOF >> $CONFIG
 EOF
 }
 
+C_LOGNAME=""
+
+# domain - vhost_name
 function vhost_start {
-LOGNAME=$(echo $1 | sed 's/[^0-9A-Za-z]*//g');
-WEBDIR=/var/www/$LOGNAME/;
+C_LOGNAME=$(echo $1 | sed 's/[^0-9A-Za-z]*//g' | awk '{print tolower($0)}');
+WEBDIR=/var/www/$2/;
 if [ ! -d "$WEBDIR" ]; then echo "web dir $WEBDIR not existing!"; exit 1; fi;
 cat <<EOF >> $CONFIG
 server {
- server_name $@;
+ server_name $1;
  listen *:80;
  listen [::]:80;
- access_log /var/log/nginx/$LOGNAME.access.log;
- error_log /var/log/nginx/$LOGNAME.error.log;
- root /var/www/$LOGNAME;
+ access_log /var/log/nginx/$C_LOGNAME.access.log;
+ error_log /var/log/nginx/$C_LOGNAME.error.log;
+ root $WEBDIR;
  index index.php index.html;
  try_files \$uri \$uri/ /index.php\$is_args\$args;
- client_max_body_size 100m;
+ client_max_body_size 1g;
 EOF
 }
 
@@ -34,17 +37,39 @@ cat <<EOF >> $CONFIG
 EOF
 }
 
+# php_version - php_config
 function php {
 php_extension $1 "fpm $PHPEXTENSIONS"
+php_fpm_pool $1 $C_LOGNAME
 PHPVALUE=$(sed "s/;/;\\\n/g" <<<"$2")
 cat <<EOF >> $CONFIG
  location ~ [^/]\.php(/|$) {
-  fastcgi_pass unix:/var/run/php/php$1-fpm.sock;
+  fastcgi_pass unix:/var/run/php/php-fpm-pool-$C_LOGNAME.sock;
   fastcgi_param PHP_VALUE "$PHPVALUE";
   fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
   include fastcgi.conf;
  }
 EOF
+}
+
+# php_version - fpm_pool_name
+function php_fpm_pool {
+POOLNAME=$(echo $2 | sed 's/[^0-9A-Za-z]*//g');
+truncate -s0 /etc/php/$1/fpm/pool.d/$POOLNAME.conf &>/dev/null || true
+cat <<EOF >> /etc/php/$1/fpm/pool.d/$POOLNAME.conf
+[$POOLNAME]
+listen = /run/php/php-fpm-pool-\$pool.sock
+listen.owner = www-data
+listen.group = www-data
+user = www-data
+group= www-data
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3
+EOF
+service php$1-fpm restart
 }
 
 function php_extension {
@@ -54,4 +79,6 @@ function php_extension {
 
 function clear_config {
  truncate -s0 $CONFIG
+ rm /etc/php/*/fpm/pool.d/*.conf 2>/dev/null || true
+ for folder in /etc/php/* ; do php_fpm_pool ${folder##*/} ${folder##*/}-default; done;
 }
--- webhosting/generate.sh
+++ webhosting/generate.sh
@@ -8,9 +8,12 @@ PHPEXTENSIONS="bcmath gd gmp json mbstring mysql sqlite3 xml zip"
 CONFIG=/etc/nginx/sites-enabled/webhosting-generated
 clear_config
 
-vhost_start html DEFAULT
-	ssl /root/ssl/ssl.crt /root/ssl/ssl.key
+vhost_start DEFAULT html
 	php 7.2 "upload_max_filesize=10M;post_max_size=200M;date.timezone=Europe/Berlin;"
+vhost_end
+
+vhost_start example.com html2/sub
+	php 7.1 "post_max_size=200M;date.timezone=Europe/Berlin;"
 vhost_end
 
 nginx -t
