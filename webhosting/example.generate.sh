1fceb5db3b92db97347a24570ae51e5feae16ff8
Jan Brodda <git@janbrodda.de>
2021-01-15 08:59:36 +0100

    update example

--- webhosting/example.generate.sh
+++ webhosting/example.generate.sh
@@ -4,18 +4,25 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/_source.sh
 
-PHPEXTENSIONS="bcmath gd gmp json mbstring mysql sqlite3 xml zip"
+PHPEXTENSIONS="bcmath gd gmp mbstring mysql sqlite3 xml zip apcu curl imagick"
 CONFIG=/etc/nginx/sites-enabled/webhosting-generated
 clear_config
 prepare_php_config
+install_packages
 
-vhost_start DEFAULT html
+php_global_config date.timezone Europe/Berlin
+php_global_config memory_limit 256M
+
+vhost_start DEFAULT default
+	php 7.4 "max_execution_time=600s;memory_limit=256M"
+	ssl /etc/ssl/certs/nginx-selfsigned.crt /etc/ssl/private/nginx-selfsigned.key
+	nginx_config default
 vhost_end
 
-vhost_start example.com html2/sub
-	php 7.3 "date.timezone=Europe/Berlin;"
+vhost_start example.com example_htdocs
+	php 8.0
 	ssl /etc/ssl/certs/nginx-selfsigned.crt /etc/ssl/private/nginx-selfsigned.key
-	nginx_config custom-part
+	ftp example-ftp-user my-secure-password example_htdocs
 vhost_end
 
 reload
