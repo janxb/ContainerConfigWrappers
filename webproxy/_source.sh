7eb6c1b3a2398abb7a8540ba45a242882c819ccf
Jan Brodda <git@janbrodda.de>
2021-04-09 10:30:26 +0200

    remove hardcoded Cloudflare IPs, pull from API instead

--- webproxy/_source.sh
+++ webproxy/_source.sh
@@ -3,6 +3,14 @@ set -e
 
 RESOLVER=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
 
+function get_cloudflare_ips {
+ IPLIST=`wget 'https://www.cloudflare.com/ips-v4' -q -O -`' '`wget 'https://www.cloudflare.com/ips-v6' -q -O -`
+for ip in $IPLIST; do
+  echo "  set_real_ip_from $ip;";
+ done
+}
+CLOUDFLARE_IPS=$(get_cloudflare_ips)
+
 function clear_file {
  rm $1 2>/dev/null || true
 }
@@ -102,29 +110,8 @@ server {
   proxy_pass_header Authorization;
   proxy_pass_header Server;
   client_max_body_size 1g;
-  set_real_ip_from 103.21.244.0/22;
-  set_real_ip_from 103.22.200.0/22;
-  set_real_ip_from 103.31.4.0/22;
-  set_real_ip_from 104.16.0.0/12;
-  set_real_ip_from 108.162.192.0/18;
-  set_real_ip_from 131.0.72.0/22;
-  set_real_ip_from 141.101.64.0/18;
-  set_real_ip_from 162.158.0.0/15;
-  set_real_ip_from 172.64.0.0/13;
-  set_real_ip_from 173.245.48.0/20;
-  set_real_ip_from 188.114.96.0/20;
-  set_real_ip_from 190.93.240.0/20;
-  set_real_ip_from 197.234.240.0/22;
-  set_real_ip_from 198.41.128.0/17;
-  set_real_ip_from 199.27.128.0/21;
-  set_real_ip_from 2400:cb00::/32;
-  set_real_ip_from 2606:4700::/32;
-  set_real_ip_from 2803:f800::/32;
-  set_real_ip_from 2405:b500::/32;
-  set_real_ip_from 2405:8100::/32;
-  set_real_ip_from 2c0f:f248::/32;
-  set_real_ip_from 2a06:98c0::/29;
   client_body_buffer_size 10m;
+$CLOUDFLARE_IPS
   real_ip_header CF-Connecting-IP;
  }
 }
